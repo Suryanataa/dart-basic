25e16cb79dff878eb511234417d54a0a7788bd6a
Surya <surya.nata.aardhana@gmail.com>
2022-06-06 13:44:59 +0700

    "operator in dart"

---
+++ increment.dart
@@ -0,0 +1,15 @@
+void main() {
+  int a = 9, b = a;
+
+// pre-encremnet
+  print('pre-increment');
+  print('Nilai a awal: $a');
+  print('++a: ${++a}');
+  print('nilai a akhir: $a');
+
+// post-increment
+  print('\npost-increment');
+  print('Nilai b awal: $b');
+  print('b++: ${b++}');
+  print('Nilai b akhir: $b');
+}
